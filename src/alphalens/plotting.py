0277a2c730d7cb5e5939b68b598a0a26dec89aab
SamZ <samzhang@usc.edu>
2024-06-13 10:19:36 +0800

    Update plotting.py

--- src/alphalens/plotting.py
+++ src/alphalens/plotting.py
@@ -179,22 +179,15 @@ def plot_turnover_table(autocorrelation_data, quantile_turnover, return_df=False
 
 def plot_information_table(ic_data, return_df=False):
     ic_summary_table = pd.DataFrame()
-    ic_summary_table["IC Mean"] = ic_data.mean()
-    ic_summary_table["IC Std."] = ic_data.std()
-    ic_summary_table["Risk-Adjusted IC"] = ic_data.mean() / ic_data.std()
-    t_stat, p_value = stats.ttest_1samp(ic_data, 0)
-    ic_summary_table["t-stat(IC)"] = t_stat
-    ic_summary_table["p-value(IC)"] = p_value
-    ic_summary_table["IC Skew"] = stats.skew(ic_data)
-    ic_summary_table["IC Kurtosis"] = stats.kurtosis(ic_data)
+    ic_summary_table["RkIC Mean"] = ic_data.mean()
+    ic_summary_table["RkIC Std."] = ic_data.std()
+    ic_summary_table["RkICIR"] = ic_data.mean() / ic_data.std()
+    # t_stat, p_value = stats.ttest_1samp(ic_data, 0)
+    # ic_summary_table["t-stat(RkIC)"] = t_stat
+    # ic_summary_table["p-value(RkIC)"] = p_value
+    # ic_summary_table["RkIC Skew"] = stats.skew(ic_data)
+    # ic_summary_table["RkIC Kurtosis"] = stats.kurtosis(ic_data)
 
-    # Calculate Rank Information Coefficient (RkIC)
-    rank_ic = ic_data.rank(pct=True)
-    ic_summary_table["RkIC Mean"] = rank_ic.mean()
-    ic_summary_table["RkIC Std."] = rank_ic.std()
-    
-    # Calculate Rank Information Ratio (RkICIR)
-    ic_summary_table["RkICIR"] = rank_ic.mean() / rank_ic.std()
     if return_df:
         return ic_summary_table
     else:
